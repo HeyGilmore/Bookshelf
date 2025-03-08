f1962dd9bb51887c589b2ad361542d058cf0c784
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-03-07 23:40:50 -0600

    added js function to add the form when button pushed

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("new-book-button").addEventListener("click", (e) => {
+  e.preventDefault;
+  document.getElementById("new-book-form").style.display = "block";
+  this.style.display = "none";
+});
+
+// document.getElementById("submitButton").addEventListener("click", () => {
+//   document.getElementById("new-book-form").style.display = "none";
+//   document.getElementById("new-book-button").style.display = "flex";
+// });
